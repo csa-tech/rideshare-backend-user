420f92c6c85415503f9ce79898e7fa6d6df780dc
jimone1 <yiyaowan@ucsb.edu>
2019-05-15 20:33:07 -0700

    test added

--- controllers/pageviewCtrl.js
+++ controllers/pageviewCtrl.js
@@ -1,3 +1,4 @@
+// if(!req.data.user_id) res.status(400);
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
@@ -19,6 +20,13 @@ connection.connect(function(err) {
 Getpersonal = function (req, res, next) { 
   try {
     connection.query(`SELECT * FROM rideshare.user_info WHERE user_id = '${req.query.user_ID}';`, function(err, rows, fields) { //这里写SQL query
+      if(!req.data.name) res.status(400).send("name not exists");
+      else if(!req.data.phoneNum) res.status(400).send("phoneNum not exists");
+      else if(!req.data.carType) res.status(400).send("carType not exists");
+      else if(!req.data.carLicense) res.status(400).send("carLicense not exists");
+      else if(!req.data.carColor) res.status(400).send("carColor not exists");
+      else if(!req.data.user_ID) res.status(400).send("user_ID not exists");
+
       if (err) { throw err; } 
       res.status(200).send(rows);
     });
@@ -32,6 +40,13 @@ InputPersonal = function(req, res, next){
     var input = req.query;
     var string = input.user_ID;
     connection.query(`SELECT * FROM rideshare.user_info WHERE user_id = '${string}';`, function(err, rows, fields) {
+      if(!req.data.name) res.status(400).send("name not exists");
+      else if(!req.data.phoneNum) res.status(400).send("phoneNum not exists");
+      else if(!req.data.carType) res.status(400).send("carType not exists");
+      else if(!req.data.carLicense) res.status(400).send("carLicense not exists");
+      else if(!req.data.carColor) res.status(400).send("carColor not exists");
+      else if(!req.data.user_ID) res.status(400).send("user_ID not exists");
+      
       if (err) {throw err;}
       var $ = req.body;
       // how to get information from response
