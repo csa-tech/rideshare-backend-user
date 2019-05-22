8de31d61ed2da965ca0b8e3d872292a18066aebb
Qinghang Hong <qinghanghong@QinghangdeMacBook-Pro.local>
2019-05-22 13:25:19 -0700

    add sql injection check

--- controllers/pageviewCtrl.js
+++ controllers/pageviewCtrl.js
@@ -19,17 +19,21 @@ connection.connect(function(err) {
 
 Getpersonal = function (req, res, next) { 
   try {
-    connection.query(`SELECT * FROM rideshare.user_info WHERE user_id = '${req.query.user_ID}';`, function(err, rows, fields) { //这里写SQL query
-      if(!req.data.name) res.status(400).send("name not exists");
-      else if(!req.data.phoneNum) res.status(400).send("phoneNum not exists");
-      else if(!req.data.carType) res.status(400).send("carType not exists");
-      else if(!req.data.carLicense) res.status(400).send("carLicense not exists");
-      else if(!req.data.carColor) res.status(400).send("carColor not exists");
-      else if(!req.data.user_ID) res.status(400).send("user_ID not exists");
+    connection.query("SELECT * FROM rideshare.user_info WHERE user_id = ?;", 
+      [
+        req.query.user_ID
+      ],
+      function(err, rows, fields) { //这里写SQL query
+        if(!req.data.name) res.status(400).send("name not exists");
+        else if(!req.data.phoneNum) res.status(400).send("phoneNum not exists");
+        else if(!req.data.carType) res.status(400).send("carType not exists");
+        else if(!req.data.carLicense) res.status(400).send("carLicense not exists");
+        else if(!req.data.carColor) res.status(400).send("carColor not exists");
+        else if(!req.data.user_ID) res.status(400).send("user_ID not exists");
 
-      if (err) { throw err; } 
-      res.status(200).send(rows);
-    });
+        if (err) { throw err; } 
+        res.status(200).send(rows);
+      });
   } catch(err) {
     res.status(500).send('SERVER ERROR:' + err);
     connection.end();
@@ -50,13 +54,16 @@ InputPersonal = function(req, res, next){
       if (err) {throw err;}
       var $ = req.body;
       // how to get information from response
-      connection.query(`UPDATE rideshare.user_info SET 
-                                                    name = '` + $.name + `',
-                                                    phoneNum = '` + $.phoneNum + `',
-                                                    carType = '` + $.carType + `',
-                                                    carLicense = '` + $.carLicense + `',
-                                                    carColor = '` + $.carColor + `' 
-                                                    WHERE user_ID = '${string}';`, function(err, rows, fields) {
+      connection.query("UPDATE rideshare.user_info SET name = ?,phoneNum = ?, carType = ?, carLicense = ?, carColor = ? WHERE user_ID = ?;",
+        [
+          name, 
+          phoneNum,
+          carType,
+          carLicense,
+          carColor,
+          string
+        ] ,
+        function(err, rows, fields) {
       if(err){throw err;}
       res.status(200).send('Success added name: ' + $.name + '\n',
                            'Success added phoneNum: ' + $.phoneNum + '\n',
