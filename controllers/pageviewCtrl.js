c5a8a9af650265d16836f2e6b42d19237786e657
Ara-yjx <jiaxi@ucsb.edu>
2019-05-29 15:46:11 -0700

    bug fix

--- controllers/pageviewCtrl.js
+++ controllers/pageviewCtrl.js
@@ -18,14 +18,20 @@ connection.connect(function(err) {
 });
 
 Getpersonal = function (req, res, next) { 
+  // Check entries in query
+  // if(!req.query.name) {
+  //   res.status(400).send("name not exists");
+  //   return;
+  // }
+  // if(!req.query.contact) {
+  //   res.status(400).send("contact not exists");
+  //   return;
+  // }
+
   try {
-    connection.query(`SELECT * FROM rideshare.user_info WHERE user_id = '${req.query.user_ID}';`, function(err, rows, fields) { //这里写SQL query
-      if(!req.data.name) res.status(400).send("name not exists");
-      else if(!req.data.contact) res.status(400).send("contact not exists");
-      // else if(!req.data.carType) res.status(400).send("carType not exists");
-      // else if(!req.data.carLicense) res.status(400).send("carLicense not exists");
-      // else if(!req.data.carColor) res.status(400).send("carColor not exists");
-      // else if(!req.data.user_ID) res.status(400).send("user_ID not exists");
+    connection.query('SELECT * FROM rideshare.user_info WHERE user_id = ?', 
+      [req.query.user_ID],
+      function(err, rows, fields) { //这里写SQL query
 
       if (err) { throw err; } 
       res.status(200).send(rows);
@@ -35,37 +41,44 @@ Getpersonal = function (req, res, next) {
     connection.end();
   }
 }
+
 InputPersonal = function(req, res, next){
   try{
     var input = req.query;
     var string = input.user_ID;
-    connection.query(`SELECT * FROM rideshare.user_info WHERE user_id = '${string}';`, function(err, rows, fields) {
-      var $ = req.body;
-      if(!req.data.name && !req.data.contact) res.status(400).send("both not exists");
-      else if(req.data.contact) connection.query(`UPDATE rideshare.user_info SET 
-                                                                              contact = '` + $.contact + `'
-                                                                              WHERE user_ID = '${string}';`, function(err, rows, fields) {
-                                                                                if(err){throw err;}
-                                                                                  res.status(200).send('Success added contact: ' + $.contact + '\n');
-                                                                                });
-      else if(req.data.name) connection.query(`UPDATE rideshare.user_info SET 
-                                                                        name = '` + $.name + `'
-                                                                        WHERE user_ID = '${string}';`, function(err, rows, fields) {
-                                                                          if(err){throw err;}
-                                                                            res.status(200).send('Success added name: ' + $.name + '\n');
-                                                                          });
-      
-      if (err) {throw err;}
-      // how to get information from response
-      connection.query(`UPDATE rideshare.user_info SET 
-                                                    name = '` + $.name + `',
-                                                    contact = '` + $.contact + `',
-                                                    WHERE user_ID = '${string}';`, function(err, rows, fields) {
-      if(err){throw err;}
-      res.status(200).send('Success added name: ' + $.name + '\n',
-                           'Success added contact: ' + $.contact + '\n');
+  
+    // if missing contact and name
+    if(!req.body.name && !req.body.contact) {
+      res.status(400).send("both not exists");
+    }
+    // if have contact only, update contact
+    else if(req.body.contact && !req.body.name) {
+      connection.query('UPDATE rideshare.user_info SET contact = ? WHERE user_id = ?', 
+        [req.body.contact, string],
+        function(err, rows, fields) {
+          if(err){throw err;}
+          res.status(200).send('Success added contact: ' + req.body.contact + '\n');
+        });
+    }
+    // if have name only, update name
+    else if(req.body.name && !req.body.contact) {
+      connection.query('UPDATE rideshare.user_info SET name = ? WHERE user_id = ?',
+        [req.body.name, string],
+        function(err, rows, fields) {
+          if(err){throw err;}
+          res.status(200).send('Success added name: ' + req.body.name + '\n');
+        });
+    }
+    // update both
+    else {
+      connection.query('UPDATE rideshare.user_info SET name = ?, contact = ? WHERE user_id = ?',
+        [req.body.name, req.body.contact, string],
+        function(err, rows, fields) {
+        if(err){throw err;}
+        res.status(200).send('Success added name: ' + req.body.name + '\n' +
+                            'Success added contact: ' + req.body.contact + '\n');
       });
-    });
+    }
   }catch(err){
     res.status(500).send("Server Error: " + err);
     connection.end();
